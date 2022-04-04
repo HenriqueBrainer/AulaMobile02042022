4f45225e51c499e0bba2a1c77006c4baa4b26bbb
HenriqueBrainer-GustavoAndrade <Henrique-Gustavo@test.com>
2022-04-04 08:16:20 -0300

    AulaPAM

---
+++ PrimeiroApp/PrimeiroApp/PrimeiroApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PrimeiroApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Clicked(object sender, EventArgs e)
+        {
+            string texto = $"O nome tem {txtNome.Text.Length} caracteres";
+            DisplayAlert("Mensagem", texto, "Ok");
+
+        }
+
+        private async void btnLimpar_Clicked(object sender, EventArgs e)
+        {
+            if(await DisplayAlert("Pergunta", "Deseja realmente Limpar?", "Yes", "No"))
+            {
+                txtNome.Text = String.Empty;
+                //Mesmo que --> txtNome.Text = "";
+            }
+        }
+    }
+}
